37514b22a127ca4a57f6903393fab81ab82723d8
Asja <anastasjamedved@gmail.com>
2025-05-12 23:54:00 +0300

    new code

--- src/main/java/ru/netology/service/Main.java
+++ src/main/java/ru/netology/service/Main.java
@@ -5,6 +5,6 @@ package ru.netology.service;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("i = " + i);
+        System.out.println();
         }
     }
