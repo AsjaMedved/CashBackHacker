5dda498b060db5bb04eccee27afab8d27944a2fd
Asja <anastasjamedved@gmail.com>
2025-05-14 16:43:43 +0300

    initial commit junit4-platform

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTestJupiter.java
@@ -0,0 +1,36 @@
+package ru.netology.service;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CashbackHackServiceTestJupiter {
+
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldReturn100IfAmount900() {
+        assertEquals(100, service.remain(900));
+    }
+
+    @Test
+    public void shouldReturn1000IfAmount0() {
+        assertEquals(1000, service.remain(0));
+    }
+
+    @Test
+    public void shouldReturn1999IfAmount1() {
+        assertEquals(1, service.remain(1999));
+    }
+
+    @Test
+    public void shouldReturn0IfAmount1000() {
+        assertEquals(0, service.remain(1000));
+    }
+
+    @Test
+    public void shouldReturn1500IfAmount500() {
+        assertEquals(500, service.remain(1500));
+    }
+}
